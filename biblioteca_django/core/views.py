de67db7b43543680b83fc0c3b00f165d2ca90d15
Belén M <100541119+bemartin94@users.noreply.github.com>
2023-09-18 20:35:05 -0300

    merge

---
+++ biblioteca_django/core/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+
+def index(request):
+    return render(request, "core/index.html")
+
+
+def altaLIbro(request):
+    context = {
+        "fecha": datetime.now(),
+    }
+    return render(request, "core/altaLibro.html", context)
+
+
+libros = {
+    1: {
+        "titulo": "Libro 1",
+        "autor": "Autor 1",
+        "anio_publicacion": 2021,
+        "imagen": "biblioteca_django\core\static\core\imagenes\images.jpeg",
+        "genero": "Ficción",
+    },
+    2: {
+        "titulo": "Libro 2",
+        "autor": "Autor 2",
+        "anio_publicacion": 2022,
+        "imagen": "\static\core\imagenes\images.jpeg",
+        "genero": "Fantasía",
+    },
+}
+
+
+def libro_detalle(request, id_libro):
+    libro = libros.get(id_libro)
+
+    if libro:
+        libro["id"] = id_libro
+        return render(request, "core/libro_detalle.html", {"libro": libro})
+    else:
+        return HttpResponse("Libro no encontrado")

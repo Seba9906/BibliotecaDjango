c657eced69ee9fbbc7b167f547b5ba5894b7c9d6
Sebastian <troncoso.seb.99@gmail.com>
2023-09-18 16:24:20 -0300

    correccion enrutamientos

--- biblioteca_django/core/urls.py
+++ biblioteca_django/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('inicio', views.index, name='index'),
+    path('inicio/', views.index, name='index'),
     path('Modificacion/AltaLibro',views.altaLIbro,name='altaLibro'),
 ]
     

3756790d0e068f1dbbe7a7ee4dfcbfbf1baaaf90
nacho83-create <116385512+nacho83-create@users.noreply.github.com>
2023-09-18 13:12:17 -0300

    modificacion de url
    se cambia la vista y su url respectiva!

--- biblioteca_django/core/urls.py
+++ biblioteca_django/core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.altaLIbro,name='altaLibro'),
+    path('Modificacion/AltaLibro',views.altaLIbro,name='altaLibro'),
 ]

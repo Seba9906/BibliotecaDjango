2fbd411b568e9248dccf00064ab3af55d43b4f44
nacho83-create <116385512+nacho83-create@users.noreply.github.com>
2023-09-30 21:10:52 -0300

    validacion formulario

--- biblioteca_django/core/urls.py
+++ biblioteca_django/core/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
     path("inicio/", views.index, name="index"),
     path("Modificacion/AltaLibro", views.altaLIbro, name="altaLibro"),
+    path("Modificacion/modificacionLibro", views.modificacionLibro, name="modificacionLibro"),
     path('prestamos', views.v_prestamos,name='prestamos'),
     path('usuario', views.usuario_perfil, name= 'usuario_perfil'),
     path("libro_detalle/<int:id_libro>/", views.libro_detalle, name="libro_detalle"),
--- biblioteca_django/core/views.py
+++ biblioteca_django/core/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.urls import reverse
 from datetime import datetime
-
+from .forms import ModificacionLibroForm
+from django.contrib import messages
 
 
 def index(request):
@@ -13,6 +15,29 @@ def altaLIbro(request):
         "fecha": datetime.now(),
     }
     return render(request, "core/altaLibro.html", context)
+
+def modificacionLibro(request):
+
+    if request.method == 'POST':
+        #instancia con datos
+        FormularioModificacion = ModificacionLibroForm(request.POST)
+        #validacion
+        if FormularioModificacion.is_valid():
+            #Mensaje de validacion
+
+            messages.info(request,"Su operacion fue ejecutada con exito")
+
+            return redirect(reverse("modificacionLibro")) 
+    
+    else: #GET    
+        FormularioModificacion = ModificacionLibroForm()
+
+    context={
+        "modificacionLibro_form" : FormularioModificacion
+
+    }
+    return render(request,"core/modificacionLibro.html", context)
+
 
 
 libros = {

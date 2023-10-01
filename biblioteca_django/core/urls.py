bd08488ef9a00a6104f2995aa66b6e9fa7d1f6e7
nacho83-create <116385512+nacho83-create@users.noreply.github.com>
2023-09-30 21:00:51 -0300

    arreglo

--- biblioteca_django/core/urls.py
+++ biblioteca_django/core/urls.py
@@ -4,7 +4,4 @@ urlpatterns = [
     path("inicio/", views.index, name="index"),
     path("Modificacion/AltaLibro", views.altaLIbro, name="altaLibro"),
     path("Modificacion/modificacionLibro", views.modificacionLibro, name="modificacionLibro"),
-    path('prestamos', views.v_prestamos,name='prestamos'),
-    path('usuario', views.usuario_perfil, name= 'usuario_perfil'),
-    path("libro_detalle/<int:id_libro>/", views.libro_detalle, name="libro_detalle"),
 ]

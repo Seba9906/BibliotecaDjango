6474204dc8891a4a4d54cac874da57dc5c65d8d9
nacho83-create <116385512+nacho83-create@users.noreply.github.com>
2023-10-18 15:56:25 -0300

    actualizacion18/10-modificacionformularios

--- biblioteca_django/core/forms.py
+++ biblioteca_django/core/forms.py
@@ -4,6 +4,31 @@ from django.core.exceptions import ValidationError
 from datetime import date  # Importa la clase 'date' de la biblioteca 'datetime'
 
 
+class AltaLibroForm(forms.Form):
+    titulo=forms.CharField(label="Titulo del Libro", required=True)
+    editorial=forms.CharField(label="Editorial", required=True)
+    Publicacion=forms.DateField(label="Fecha de Publicación",widget=forms.SelectDateWidget(years=range(1800, 2024)))
+    autor=forms.CharField(label="Autor", required=True)
+    Stock=forms.IntegerField(label="Cantidad de Unidades")
+    genero=forms.CharField(label="Genero Literario", required=True)
+    Modificacion=forms.DateTimeField(label="Fecha y Hora Modificacion",widget=forms.TextInput(attrs={'readonly': 'readonly','class':'labelModificacion'}),initial=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    caratula=forms.ImageField(label="Imagen de Portada", required= None)
+
+    def clean_Stock(self):
+        if self.cleaned_data["Stock"] > 20:
+            raise ValidationError("El stock maximo para cada libro es de 20 unidades por titulo")
+        
+        return self.cleaned_data["Stock"] 
+    
+    def clean (self):
+        if self.cleaned_data["titulo"] == "Cronica de una muerte anunciada":
+            raise ValidationError("El libro seleccionado ya existe")
+        
+
+        return self.cleaned_data  
+
+
+
 class ModificacionLibroForm(forms.Form):
     titulo=forms.CharField(label="Titulo del Libro", required=True)
     editorial=forms.CharField(label="Editorial", required=True)
@@ -26,7 +51,9 @@ class ModificacionLibroForm(forms.Form):
         
 
         return self.cleaned_data  
-    
+
+
+
 class PrestamoForm(forms.Form):
     titulo = forms.CharField(label='Título del Libro', max_length=100, required=True)
     autor = forms.CharField(label='Autor', max_length=100, required=True)

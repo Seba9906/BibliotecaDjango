235b2752337696af0ebbe138c59f475d4facce19
nacho83-create <116385512+nacho83-create@users.noreply.github.com>
2023-11-05 20:16:49 -0300

    05/11 modificacion en admin

--- biblioteca_django/core/admin.py
+++ biblioteca_django/core/admin.py
@@ -1,8 +1,31 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.fields.related import ManyToManyField
+from django.forms.models import ModelMultipleChoiceField
+from django.http.request import HttpRequest
 from core.models import Usuario,Autor,Libro,Prestamo
 
-admin.site.register(Usuario)
-admin.site.register(Autor)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('dni' , 'apellido','nombre', 'email', 'contraseña')
+    list_editable = ('apellido', 'nombre', 'email', 'contraseña')
+    list_display_links=['dni']
+    search_fields = ['apellido']
+
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nombre','nacimiento', 'pais')
+    list_editable = ('nacimiento', 'pais')
+    list_display_links=['nombre']
+    search_fields = ['nombre']
+
+class LibroAdmin(admin.ModelAdmin):
+    list_display=('titulo', 'editorial','publicacion','genero') 
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == 'autores':
+            kwargs['queryset'] = Autor.objects.filter(nacimiento__startswith='1').orderby('nombre')
+        return super().formfield_for_manytomany(db_field, request, **kwargs)       
+
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Autor, AutorAdmin)
 admin.site.register(Libro)
 admin.site.register(Prestamo)
 
